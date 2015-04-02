7358b728128520d80fab809cdbb26f219c1c8fb3
Gelidaer <rajieenthan@gmail.com>
2015-04-02 13:26:00 -0400

    Create Spawn.cs

---
+++ Spawn.cs
@@ -0,0 +1,22 @@
+# Spawn
+
+public class Spawn : MonoBehaviour {
+    public GameObject player;
+    public GameObject origPlayer;
+    public int noPlayers = 4;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(origPlayer);
+        for (int i = 0; i < noPlayers; i++)
+        {
+            Instantiate(player, new Vector3(0, 1, -i-1), Quaternion.identity);
+            player.GetComponent<Movement2>().enabled = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
